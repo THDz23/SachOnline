9a892b41412fe547e8197ca4bf2ba8754eae3d39
Hùng Trịnh <trinhhung23102003@gmail.com>
2023-10-21 15:22:39 +0700

    chi tiết sách và xóa admin

--- SachOnline/Areas/Admin/Controllers/SachController.cs
+++ SachOnline/Areas/Admin/Controllers/SachController.cs
@@ -27,8 +27,52 @@ namespace SachOnline.Areas.Admin.Controllers
             ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
             return View();
         }
-        
-
+        public ActionResult Details(int id)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if(sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id,FormCollection f)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if(sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            var cthdh = db.CHITIETDATHANGs.Where(ct => ct.MaSach == id);
+            if(cthdh.Count() > 0)
+            {
+                ViewBag.ThongBao = "Sách này đang có trong bảng chi tiết đặt hàng <br>" + "Nếu muốn xóa thì phải xóa hết mã sách trong bảng chi tiết đặt hàng";
+                return View(sach);
+            }
+            var vietsach = db.VIETSACHes.Where(vs => vs.MaSach == id).ToList();
+            if(vietsach != null)
+            {
+                db.VIETSACHes.DeleteAllOnSubmit(vietsach);
+                db.SubmitChanges();
+            }
+            db.SACHes.DeleteOnSubmit(sach);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
     }
     
 }
