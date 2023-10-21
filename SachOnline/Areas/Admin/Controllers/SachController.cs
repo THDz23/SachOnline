57a45b299cf01d77e03fd594f99e16e3d2f7da35
Hùng Trịnh <trinhhung23102003@gmail.com>
2023-10-21 16:00:52 +0700

    lay code dev1 de chay

--- SachOnline/Areas/Admin/Controllers/SachController.cs
+++ SachOnline/Areas/Admin/Controllers/SachController.cs
@@ -27,6 +27,50 @@ namespace SachOnline.Areas.Admin.Controllers
             ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
             return View();
         }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(SACH sach, FormCollection f, HttpPostedFileBase fFileUpload)
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+
+            if (fFileUpload == null)
+            {
+                ViewBag.ThongBao = "Hãy chọn ảnh bìa.";
+                ViewBag.TenSach = f["sTenSach"];
+                ViewBag.MoTa = f["sMoTa"];
+                ViewBag.SoLuong = int.Parse(f["iSoLuong"]);
+                ViewBag.GiaBan = decimal.Parse(f["mGiaBan"]);
+                ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe", int.Parse(f["MaCD"]));
+                ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", int.Parse(f["MaNXB"]));
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var sFileName = Path.GetFileName(fFileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Image"), sFileName);
+                    if (!System.IO.File.Exists(path))
+                    {
+                        fFileUpload.SaveAs(path);
+                    }
+                    sach.TenSach = f["sTenSach"];
+                    sach.MoTa = f["sMoTa"];
+                    sach.AnhBia = sFileName;
+                    sach.NgayCapNhat = Convert.ToDateTime(f["dNgayCapNhat"]);
+                    sach.SoLuongBan = int.Parse(f["iSoLuong"]);
+                    sach.GiaBan = decimal.Parse(f["mGiaBan"]);
+                    sach.MaCD = int.Parse(f["MaCD"]);
+                    sach.MaNXB = int.Parse(f["MaNXB"]);
+                    db.SACHes.InsertOnSubmit(sach);
+                    db.SubmitChanges();
+                    return RedirectToAction("Index");
+
+                }
+                return View();
+            }
+        }
         public ActionResult Details(int id)
         {
             var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
@@ -72,6 +116,19 @@ namespace SachOnline.Areas.Admin.Controllers
             db.SACHes.DeleteOnSubmit(sach);
             db.SubmitChanges();
             return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe", sach.MaCD);
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", sach.MaNXB);
+            return View(sach);
         }
     }
     
